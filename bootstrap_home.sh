10294ec1daa7d11625d9afd1f8102e321ea61f46
chasehippen <chase.d.hippen@gmail.com>
2022-12-15 11:57:48 -0800

    init commit

---
+++ bootstrap_home.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install Ansible
+python3 -m pip install ansible
+ansible-galaxy collection install community.general
+
+# Run playbook
+ansible-playbook playbook.yaml --diff --tags home
