bf75a87e47f8c749dbf4c769836d157ca0a958bd
chasehippen <chase@hippen.org>
2022-12-23 22:14:11 -0800

    removing ansible, changing to bash script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,34 +1,55 @@
 #!/bin/bash
 
-# Install curl
-if ! which curl > /dev/null
-then
-  echo "installing curl"
-  sudo apt install curl
-else
-  echo "curl already installed"
-fi
+# Function to check for ppa before installing
+add_ppa() {
+  for i in "$@"; do
+    grep -h "^deb.*$i" /etc/apt/sources.list.d/* > /dev/null 2>&1
+    if [ $? -ne 0 ]
+    then
+      echo "Adding ppa:$i"
+      sudo add-apt-repository -y ppa:$i
+    else
+      echo "ppa:$i already exists"
+    fi
+  done
+}
 
-# Install pip
-if ! command -v pip > /dev/null
-then
-  echo "installing pip"
-  curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
-  python3 /tmp/get-pip.py
-else
-  echo "pip already installed."
-fi
+# Add alacritty ppa
+add_ppa aslatter/ppa
 
-# Install Ansible
-if ! which ansible > /dev/null 
-then
-  echo "installing ansible"
-  python3 -m pip install ansible
-  ansible-galaxy collection install community.general
-  ansible galaxy collection install ansible.posix
-else
-  echo "ansible already installed."
-fi
+# Install packages with apt
+cat apt | xargs sudo apt install -y
 
-# Run playbook
-ansible-playbook playbook.yaml --diff -K
+# Setup zsh
+chsh -s $(which zsh)
+
+# ohmyzsh
+[[ -z "$ZSH" ]] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || echo "ohmyzsh already installed"
+
+# Configure powerlevel10k & fonts
+mkdir -p $HOME/.local/share/fonts
+cd $HOME/.local/share/fonts
+curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -Lso 'MesloLGS_NF_Regular.ttf'
+curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -Lso 'MesloLGS_NF_Bold.ttf'
+curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -Lso 'MesloLGS_NF_Italic.ttf'
+curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -Lso 'MesloLGS_NF_Bold_Italic.ttf'
+cd -
+fc-cache -f
+
+# Configure Alacritty
+mkdir -p $HOME/.config && mkdir -p $HOME/.config/alacritty && cp configs/alacritty/alacritty.yml $HOME/.config/alacritty
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/alacritty 50
+sudo update-alternatives --config x-terminal-emulator
+
+git clone --depth=1 'https://github.com/romkatv/powerlevel10k.git' ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k 2> /dev/null || git -C ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k pull
+cp configs/zsh/zshrc ~/.zshrc
+
+# zsh-autosuggestions
+git clone 'https://github.com/zsh-users/zsh-autosuggestions.git' ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 2> /dev/null || git -C ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions pull
+
+# Install packages with snap
+cat snap | xargs -L 1 sudo snap install --classic
+
+# Install packages with curl
+# minikube
+
--- configs/nvim/lua/config/windows.lua
+++
@@ -1,4 +0,0 @@
-vim.api.nvim_set_keymap('n', '<leader>wm', [[<cmd>:WindowsMaximize<CR>]], { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<leader>we', [[<cmd>:WindowsEnableAutowidth<CR>]], { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<leader>wd', [[<cmd>:WindowsDisableAutowidth<CR>]], { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<leader>wt', [[<cmd>:WindowsToggleAutowidth<CR>]], { noremap = true, silent = true })
